b30927fbf1569e0afe3e9c71bfaba7709979ea26
Natnael Mamuye <naty.buna@gmail.com>
2025-06-06 19:44:56 +0300

    code modified

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,15 +1,1 @@
-CREATE TABLE Banks (
-    bank_id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    bank_name VARCHAR2(100) NOT NULL
-);
-
-CREATE TABLE Reviews (
-    review_id NUMBER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    review_text CLOB NOT NULL,
-    rating NUMBER NOT NULL,
-    sentiment_label VARCHAR2(20),
-    sentiment_score FLOAT,
-    review_date DATE,
-    bank_id NUMBER,
-    FOREIGN KEY (bank_id) REFERENCES Banks(bank_id)
-);
+cursor.execute("SELECT dbms_lob.substr(review_text, 100, 1), rating, review_date FROM Reviews WHERE ROWNUM <= 3")
